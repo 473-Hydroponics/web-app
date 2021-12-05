599843404cad5dedd8fa10aed9be99bf864d9a86
Cole <cmhudson11@gmail.com>
2021-12-05 18:01:32 -0500

    website structure finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -42,42 +42,83 @@ class HydroponicsTable extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <table>
-        <thead>
-        <tr>
-          <th>Time</th>
-          <th>pH</th>
-          <th>tds</th>
-          <th>Water Temp</th>
-          <th>Air Temp</th>
-          <th>Humidity</th>
-        </tr>
-        </thead>
-        <tbody>
-          {items.map(item => (
-            <tr>
-            <td key="time">
-             {item.requestDateTime}
-            </td>
-            <td key="ph">
-              {item.pH}
-            </td>
-            <td key="tds">
-              {item.TDS}
-            </td>
-            <td key="Water Temp">
-              {item["Water Temp"]}
-            </td>
-            <td key="Air Temp">
-              {item["Air Temp"]}
-            </td>
-            <td key="humidity">
-              {item.Humidity}
-            </td>
-            </tr>
-          ))}
-        </tbody>
-        </table>
+        <div>
+          <b><label htmlFor="ids">Sensor module ID: </label></b>
+          <select name="ids" id="ids">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+
+          <h3>Current Values</h3>
+
+          <table className="center">
+          <thead>
+          <tr>
+            <th>pH</th>
+            <th>tds</th>
+            <th>Water Temp</th>
+            <th>Air Temp</th>
+            <th>Humidity</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>5</td>
+            <td>15</td>
+            <td>123</td>
+            <td>1512</td>
+            <td>8</td>
+          </tr>
+          </tbody>
+          </table>
+
+          <h3>Graphs</h3>
+
+          <label htmlFor="ids">Time Range: </label>
+          <select name="timeRange" id="timeRange">
+            <option value="hour">hour</option>
+            <option value="day">day</option>
+            <option value="week">week</option>
+          </select>
+
+          <h3>All Values</h3>
+          <table className="center">
+          <thead>
+          <tr>
+            <th>Time</th>
+            <th>pH</th>
+            <th>tds</th>
+            <th>Water Temp</th>
+            <th>Air Temp</th>
+            <th>Humidity</th>
+          </tr>
+          </thead>
+          <tbody>
+            {items.map(item => (
+              <tr>
+              <td key="time">
+               {item.requestDateTime}
+              </td>
+              <td key="ph">
+                {item.pH.toFixed(2)}
+              </td>
+              <td key="tds">
+                {item.TDS.toFixed(2)}
+              </td>
+              <td key="Water Temp">
+                {item["Water Temp"].toFixed(2)}
+              </td>
+              <td key="Air Temp">
+                {item["Air Temp"].toFixed(2)}
+              </td>
+              <td key="humidity">
+                {item.Humidity.toFixed(2)}
+              </td>
+              </tr>
+            ))}
+          </tbody>
+          </table>
+        </div>
       );
     }
   }
@@ -88,6 +129,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
+      <h1>Hydroponics</h1>
+      <p>For EECS 473. Created by Peter Hammel, Cole Hudson, Tanvi Jivtode, Ashvin Kumar, Brian Oo, and XiaoXue Zhong</p>
       <HydroponicsTable/>
     </div>
   );
