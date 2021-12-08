2d85e0efa5a3adef4e80c4f0feffb7e7c1f5aa75
Cole <cmhudson11@gmail.com>
2021-12-08 18:13:53 -0500

    dynamically update site depending on time range and sensor id

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,140 +16,19 @@ function compare( a, b ) {
   return 0;
 }
 
-class HydroponicsCurrentValuesTable extends React.Component {
-  constructor(props) {
-    super(props);
-
-    //get props.id here to get the id of our sensor
-
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://oylpp760bf.execute-api.us-east-2.amazonaws.com/prod/473hydrodata")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div>
-          <b><label htmlFor="ids">Sensor module ID: </label></b>
-          <select name="ids" id="ids">
-            <option value="1">1</option>
-            <option value="2">2</option>
-          </select>
-
-          <h3>Current Values</h3>
-
-          <table className="center">
-          <thead>
-          <tr>
-            <th>pH</th>
-            <th>tds</th>
-            <th>Water Temp</th>
-            <th>Air Temp</th>
-            <th>Humidity</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td>5</td>
-            <td>15</td>
-            <td>123</td>
-            <td>1512</td>
-            <td>8</td>
-          </tr>
-          </tbody>
-          </table>
-
-          <h3>Graphs</h3>
-
-          <label htmlFor="ids">Time Range: </label>
-          <select name="timeRange" id="timeRange">
-            <option value="hour">hour</option>
-            <option value="day">day</option>
-            <option value="week">week</option>
-          </select>
-
-          <h3>All Values</h3>
-          <table className="center">
-          <thead>
-          <tr>
-            <th>Time</th>
-            <th>pH</th>
-            <th>tds</th>
-            <th>Water Temp</th>
-            <th>Air Temp</th>
-            <th>Humidity</th>
-          </tr>
-          </thead>
-          <tbody>
-            {items.map(item => (
-              <tr>
-              <td key="time">
-               {item.requestDateTime}
-              </td>
-              <td key="ph">
-                {item.pH.toFixed(2)}
-              </td>
-              <td key="tds">
-                {item.TDS.toFixed(2)}
-              </td>
-              <td key="Water Temp">
-                {item["Water Temp"].toFixed(2)}
-              </td>
-              <td key="Air Temp">
-                {item["Air Temp"].toFixed(2)}
-              </td>
-              <td key="humidity">
-                {item.Humidity.toFixed(2)}
-              </td>
-              </tr>
-            ))}
-          </tbody>
-          </table>
-        </div>
-      );
-    }
-  }
-}
-
-
 class HydroponicsTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
+      items: [],
+      hourDifference: 24,
+      sensorId: 1
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeSensor = this.handleChangeSensor.bind(this);
   }
 
   componentDidMount() {
@@ -159,7 +38,8 @@ class HydroponicsTable extends React.Component {
         (result) => {
           this.setState({
             isLoaded: true,
-            items: result.items
+            items: result.items,
+            hourDifference: 24
           });
         },
         // Note: it's important to handle errors here
@@ -173,6 +53,20 @@ class HydroponicsTable extends React.Component {
         }
       )
   }
+
+  handleChange(event){
+    if(event.target.value == "day"){
+      this.setState({hourDifference: 24});
+    }else if(event.target.value == "hour"){
+      this.setState({hourDifference: 2});
+    }else if(event.target.value == "week"){
+      this.setState({hourDifference: 168});
+    }
+  };
+
+  handleChangeSensor(event){
+      this.setState({sensorId: event.target.value});
+  };
 
   render() {
     const { error, isLoaded, items } = this.state;
@@ -194,18 +88,23 @@ class HydroponicsTable extends React.Component {
       var sorted_objects = [];
 
       for(var i = 0; i < items.length; i++){
-        if(items[i].id == id){
+        if(items[i].id == this.state.sensorId){
           //console.log(items[i].RequestTime);
           //time.setTime(Date.parse(items[i].RequestTime));
           var time = new Date();
           time.setTime(Date.parse(items[i].RequestTime));
-          id_items_ph.push([time, items[i].pH]);
-          id_items_tds.push([time, items[i].TDS]);
-          id_items_waterTemp.push([time, items[i]["Water Temp"]]);
-          id_items_airTemp.push([time, items[i]["Air Temp"]]);
-          id_items_humidity.push([time, items[i].Humidity]);
 
-          sorted_objects.push([time, items[i]]);
+          var now = new Date();
+          var hoursDif = Math.abs(now - time) / 36e5;
+          if(hoursDif < this.state.hourDifference){
+            id_items_ph.push([time, items[i].pH]);
+            id_items_tds.push([time, items[i].TDS]);
+            id_items_waterTemp.push([time, items[i]["Water Temp"]]);
+            id_items_airTemp.push([time, items[i]["Air Temp"]]);
+            id_items_humidity.push([time, items[i].Humidity]);
+
+            sorted_objects.push([time, items[i]]);
+          }
         }
       }
       //sort
@@ -224,13 +123,15 @@ class HydroponicsTable extends React.Component {
       id_items_airTemp.unshift(['time', 'Air Temp']);
       id_items_humidity.unshift(['time', 'Humidity']);
 
+      var newest_item = sorted_objects[sorted_objects.length - 1];
 
       return (
         <div>
+        <div>
           <b><label htmlFor="ids">Sensor module ID: </label></b>
-          <select name="ids" id="ids">
-            <option value="1">1</option>
-            <option value="2">2</option>
+          <select name="ids" id="ids" value={this.state.sensorId} onChange={this.handleChangeSensor}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
           </select>
 
           <h3>Current Values</h3>
@@ -246,17 +147,29 @@ class HydroponicsTable extends React.Component {
           </tr>
           </thead>
           <tbody>
+          {newest_item &&
           <tr>
-            <td>5</td>
-            <td>15</td>
-            <td>123</td>
-            <td>1512</td>
-            <td>8</td>
+
+              <td>{newest_item[1]["pH"].toFixed(2)}</td>
+              <td>{newest_item[1]["TDS"].toFixed(2)}</td>
+              <td>{newest_item[1]["Water Temp"].toFixed(2)}</td>
+              <td>{newest_item[1]["Air Temp"].toFixed(2)}</td>
+              <td>{newest_item[1]["Humidity"].toFixed(2)}</td>
           </tr>
+          }
           </tbody>
           </table>
 
+          </div>
+
           <h3>Graphs</h3>
+
+          <label htmlFor="ids">Time Range: </label>
+          <select name="timeRange" id="timeRange" value={this.state.selected} onChange={this.handleChange}>
+            <option value="hour">hour</option>
+            <option value="day">day</option>
+            <option value="week">week</option>
+          </select>
 
           <table className="center">
           <tbody>
@@ -269,6 +182,10 @@ class HydroponicsTable extends React.Component {
                 loader={<div>Loading Chart</div>}
                 data={id_items_ph}
                 options={{
+                  series:{
+                    0: {color: '#e2431e'}
+                  },
+                  title: 'pH',
                   hAxis: {
                     title: 'Time',
                   },
@@ -288,6 +205,10 @@ class HydroponicsTable extends React.Component {
                 loader={<div>Loading Chart</div>}
                 data={id_items_tds}
                 options={{
+                  series:{
+                    0: {color: '#e7711b'}
+                  },
+                  title: 'TDS',
                   hAxis: {
                     title: 'Time',
                   },
@@ -308,6 +229,10 @@ class HydroponicsTable extends React.Component {
                 loader={<div>Loading Chart</div>}
                 data={id_items_waterTemp}
                 options={{
+                  series:{
+                    0: {color: '#f1ca3a'}
+                  },
+                  title: 'Water Temperature',
                   hAxis: {
                     title: 'Time',
                   },
@@ -327,6 +252,10 @@ class HydroponicsTable extends React.Component {
                 loader={<div>Loading Chart</div>}
                 data={id_items_airTemp}
                 options={{
+                  series:{
+                    0: {color: '#6f9654'}
+                  },
+                  title: 'Air Temperature',
                   hAxis: {
                     title: 'Time',
                   },
@@ -347,6 +276,10 @@ class HydroponicsTable extends React.Component {
                 loader={<div>Loading Chart</div>}
                 data={id_items_humidity}
                 options={{
+                  series:{
+                    0: {color: '#1c91c0'}
+                  },
+                  title: 'Humidity',
                   hAxis: {
                     title: 'Time',
                   },
@@ -360,14 +293,6 @@ class HydroponicsTable extends React.Component {
             </tr>
           </tbody>
           </table>
-
-
-          <label htmlFor="ids">Time Range: </label>
-          <select name="timeRange" id="timeRange">
-            <option value="hour">hour</option>
-            <option value="day">day</option>
-            <option value="week">week</option>
-          </select>
 
           <h3>All Values</h3>
           <table className="center">
