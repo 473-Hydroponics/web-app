a9eea99379534b0298fa81e7105b7f1c0e27eac0
Cole <cmhudson11@gmail.com>
2021-12-08 19:10:56 -0500

    bootstrap update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,6 +18,22 @@ function compare( a, b ) {
   return 0;
 }
 
+const selectStyle = {
+  width: 130
+};
+
+const floatLeftStyle = {
+  float: "left"
+}
+
+const logoStyle = {
+  "padding-left": 20
+}
+
+const namePadding = {
+  "padding-right": 20
+}
+
 class HydroponicsTable extends React.Component {
   constructor(props) {
     super(props);
@@ -120,7 +136,7 @@ class HydroponicsTable extends React.Component {
       sorted_objects.sort(compare);
 
       id_items_ph.unshift(['time', 'pH']);
-      id_items_tds.unshift(['time', 'tds']);
+      id_items_tds.unshift(['time', 'EC']);
       id_items_waterTemp.unshift(['time', 'Water Temp']);
       id_items_airTemp.unshift(['time', 'Air Temp']);
       id_items_humidity.unshift(['time', 'Humidity']);
@@ -130,48 +146,73 @@ class HydroponicsTable extends React.Component {
       return (
         <div>
         <div>
-          <b><label htmlFor="ids">Sensor module ID: </label></b>
-          <select name="ids" id="ids" value={this.state.sensorId} onChange={this.handleChangeSensor}>
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-          </select>
 
-          <h3>Current Values</h3>
+          <div class="container py-3">
+          <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal">Current Values</h4>
+                </div>
+                <div class="card-body">
+                <table className="center table">
+                <thead>
+                <tr>
+                  <th>pH</th>
+                  <th>Electrical Conductivity</th>
+                  <th>Water Temp</th>
+                  <th>Air Temp</th>
+                  <th>Humidity</th>
+                </tr>
+                </thead>
+                <tbody>
+                {newest_item &&
+                <tr>
 
-          <table className="center">
-          <thead>
-          <tr>
-            <th>pH</th>
-            <th>tds</th>
-            <th>Water Temp</th>
-            <th>Air Temp</th>
-            <th>Humidity</th>
-          </tr>
-          </thead>
-          <tbody>
-          {newest_item &&
-          <tr>
-
-              <td>{newest_item[1]["pH"].toFixed(2)}</td>
-              <td>{newest_item[1]["TDS"].toFixed(2)}</td>
-              <td>{newest_item[1]["Water Temp"].toFixed(2)}</td>
-              <td>{newest_item[1]["Air Temp"].toFixed(2)}</td>
-              <td>{newest_item[1]["Humidity"].toFixed(2)}</td>
-          </tr>
-          }
-          </tbody>
-          </table>
-
+                    <td>{newest_item[1]["pH"].toFixed(2)}</td>
+                    <td>{newest_item[1]["TDS"].toFixed(2)}</td>
+                    <td>{newest_item[1]["Water Temp"].toFixed(2)}</td>
+                    <td>{newest_item[1]["Air Temp"].toFixed(2)}</td>
+                    <td>{newest_item[1]["Humidity"].toFixed(2)}</td>
+                </tr>
+                }
+                </tbody>
+                </table>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal">Time Range</h4>
+                </div>
+                <div class="card-body">
+                  <select class="form-select center" style={selectStyle} name="timeRange" id="timeRange" value={this.state.selected} onChange={this.handleChange}>
+                    <option value="hour">hour</option>
+                    <option value="day">day</option>
+                    <option value="week">week</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal">Sensor Module ID</h4>
+                </div>
+                  <div class="card-body">
+                  <select class="form-select center"  style={selectStyle} name="ids" id="ids" value={this.state.sensorId} onChange={this.handleChangeSensor}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
           </div>
 
           <h3>Graphs</h3>
-
-          <label htmlFor="ids">Time Range: </label>
-          <select name="timeRange" id="timeRange" value={this.state.selected} onChange={this.handleChange}>
-            <option value="hour">hour</option>
-            <option value="day">day</option>
-            <option value="week">week</option>
-          </select>
 
           <table className="center">
           <tbody>
@@ -210,12 +251,12 @@ class HydroponicsTable extends React.Component {
                   series:{
                     0: {color: '#e7711b'}
                   },
-                  title: 'TDS',
+                  title: 'Electrical Conductivity',
                   hAxis: {
                     title: 'Time',
                   },
                   vAxis: {
-                    title: 'tds',
+                    title: 'EC',
                   },
                 }}
                 rootProps={{ 'data-testid': '1' }}
@@ -297,7 +338,9 @@ class HydroponicsTable extends React.Component {
           </table>
 
           <h3>All Values</h3>
-          <table className="center">
+
+          <div class="container py-3">
+          <table className="center table">
           <thead>
           <tr>
             <th>Time</th>
@@ -333,6 +376,7 @@ class HydroponicsTable extends React.Component {
             ))}
           </tbody>
           </table>
+          </div>
         </div>
       );
     }
@@ -343,8 +387,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+          <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+            <img src="hydroIgrow.png" alt="logo" style={logoStyle} width="200"/>
+          </a>
+
+          <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+            <span class="fs-4" style={namePadding}>EECS 473 Team 16: Hydroponics</span>
+          </nav>
+        </div>
       </header>
-      <img src="hydroIgrow.png" alt="logo" width="500"/>
       <p>For EECS 473. Created by Peter Hammel, Cole Hudson, Tanvi Jivtode, Ashvin Kumar, Brian Oo, and XiaoXue Zhong</p>
       <HydroponicsTable/>
     </div>
